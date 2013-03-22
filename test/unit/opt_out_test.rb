bf1bcbe6dc9cc081f61e3134ae6fa62725496948
Jerry Cheung <jch@whatcodecraves.com>
2013-03-22 16:07:19 -0700

    move tests to it's own file

---
+++ test/unit/opt_out_test.rb
@@ -0,0 +1,70 @@
+# test_helper
+require 'opt_out'
+
+STORE = {}  # shared memory store for persistence. Cleared between tests
+
+OptOut.configure do |c|
+  c.persistence = {
+    :adapter => OptOut::Persistence::MemoryAdapter,
+    :options => {
+      :store => STORE
+    }
+  }
+end
+
+module OptOut
+  class PersistenceTest < Test::Unit::TestCase
+    Model = Struct.new(:id, :email) do
+      include OptOut::Persistence
+
+      def serializable_hash
+        {:email => email}
+      end
+    end
+
+    def setup
+      STORE.clear
+      @instance = Model.new(:id => 'model_1', :email => 'jollyjerry@gmail.com')
+      @instance.save
+    end
+
+    def test_save_and_find
+      record = Model.find(@instance.id)
+      assert_not_nil record
+      assert_equal @instance.email, record.email
+    end
+
+    def test_destroy_and_find
+      @instance.destroy
+      assert_nil Model.find(@instance.id)
+    end
+  end
+
+  class ListTest < Test::Unit::TestCase
+    def setup
+      STORE.clear
+      @list = List['security']
+    end
+
+    def test_lookup
+      assert @list.is_a?(List)
+      assert_equal 'security', @list.name
+    end
+
+    def test_subscribe
+      @list.subscribe('user_1')
+      assert @list.subscribed?('user_1')
+    end
+
+    def test_unsubscribe
+      @list.unsubscribe('user_1')
+      assert !@list.subscribed?('user_1')
+    end
+
+    def test_resubscribe
+      @list.unsubscribe('user_1')
+      @list.subscribe('user_1')
+      assert @list.subscribed?('user_1')
+    end
+  end
+end
